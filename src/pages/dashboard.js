366c6c93fe89f4853d2e1bf0fc663c97e97d439f
Max Yuan <maxyuan6717@gmail.com>
2021-04-07 02:23:53 -0400

    link to today's entry from dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getUser } from "../util/api";
 import styles from "./dashboard.module.css";
+import { Row } from "react-bootstrap";
+
 const Dashboard = () => {
   const { id } = useParams();
   const [info, setInfo] = useState({});
@@ -17,8 +19,19 @@ const Dashboard = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div>Hello {info.email}</div>
-        <div>Welcome to day {info.day}</div>
+        <div className="header">Hello {info.email}</div>
+        <div className="subheader">
+          <Row className="mx-auto">
+            <span className="my-auto">Welcome to day {info.day}</span>
+            <span className="mx-3 my-auto">|</span>
+            <Link
+              className={styles.entry_link + " my-auto"}
+              to={`/${id}/${info.day}`}
+            >
+              Fill out today's entry →
+            </Link>
+          </Row>
+        </div>
       </div>
     </div>
   );

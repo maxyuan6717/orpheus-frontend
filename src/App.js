42378c03e7deac5e52ad86773e6bbc05d9ff9fb9
Max Yuan <maxyuan6717@gmail.com>
2021-04-09 12:20:45 -0400

    set up basic burger menu

--- src/App.js
+++ src/App.js
@@ -7,25 +7,49 @@ import DayPage from "./pages/day";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import { slide as Menu } from "react-burger-menu";
+import { NavLink } from "react-router-dom";
+import "./common/burger.css";
 
 import "./common/typography.css";
 
 function App() {
+  const MyRoute = ({ children, ...otherProps }) => {
+    return (
+      <Route {...otherProps}>
+        <div className={styles.container}>
+          <div className={styles.inner}>{children}</div>
+        </div>
+      </Route>
+    );
+  };
+
   return (
     <Provider store={store}>
-      <div className={styles.container}>
-        <div className={styles.inner}>
-          <Router>
-            <Switch>
-              <Route exact path="/about" component={AboutPage} />
-              <Route exact path="/faq" component={FAQPage} />
-              <Route exact path="/:id" component={Dashboard} />
-              <Route exact path="/:id/:day_no" component={DayPage} />
-              <Route path="/" component={Landing} />
-            </Switch>
-          </Router>
-        </div>
-      </div>
+      <Router>
+        <Menu>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/faq">FAQ</NavLink>
+        </Menu>
+        <Switch>
+          <MyRoute exact path="/about">
+            <AboutPage />
+          </MyRoute>
+          <MyRoute exact path="/faq">
+            <FAQPage />
+          </MyRoute>
+          <MyRoute exact path="/:id">
+            <Dashboard />
+          </MyRoute>
+          <MyRoute exact path="/:id/:day_no">
+            <DayPage />
+          </MyRoute>
+          <MyRoute path="/">
+            <Landing />
+          </MyRoute>
+        </Switch>
+      </Router>
     </Provider>
   );
 }

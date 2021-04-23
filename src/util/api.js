50023cb57d7775801aa3de61b4fabe5955abe0dd
Max Yuan <maxyuan6717@gmail.com>
2021-04-23 01:33:50 -0400

    disable add user

--- src/util/api.js
+++ src/util/api.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 import { Base } from "./base";
 
-const addUser = async (email) => {
-  let user = await axios.post(`${Base}/user/add`, { email });
-  return user;
-};
-
 const addBeta = async (email) => {
   let beta = await axios.post(`${Base}/beta/add`, { email });
   return beta;
@@ -21,4 +16,4 @@ const registerUser = async (email, name, password1, password2) => {
   return user;
 };
 
-export { addUser, addBeta, registerUser };
+export { addBeta, registerUser };

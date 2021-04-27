6c399081505e562765e1397af2be6e11f740fe2a
Max Yuan <maxyuan6717@gmail.com>
2021-04-27 13:47:28 -0400

    update thankyou

--- src/components/thankyou.js
+++ src/components/thankyou.js
@@ -1,6 +1,8 @@
 import styles from "./thankyou.module.css";
 import { Row } from "react-bootstrap";
 import { HiMail } from "react-icons/hi";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaLightbulb } from "react-icons/fa";
 
 const ThankYou = () => {
   return (
@@ -12,9 +14,37 @@ const ThankYou = () => {
           activities. If you have any feedback, email us!
         </div>
         <Row className={`mx-auto justify-content-center ${styles.btn_row}`}>
-          <a href="mailto: orpheuspledge@gmail.com">
+          <a className="mx-3" href="mailto: orpheuspledge@gmail.com">
             <div className={`${styles.btn_container} d-flex`}>
               <HiMail
+                className="m-auto"
+                size={35}
+                style={{ display: "block" }}
+              />
+            </div>
+          </a>
+          <a
+            className="mx-3"
+            href="https://www.instagram.com/orpheus.pledge/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <div className={`${styles.btn_container} d-flex`}>
+              <AiFillInstagram
+                className="m-auto"
+                size={35}
+                style={{ display: "block" }}
+              />
+            </div>
+          </a>
+          <a
+            className="mx-3"
+            href="https://www.notion.so/Tips-Resources-6d7d8bcfea8a481d9cd3c6cb13f8dc67"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <div className={`${styles.btn_container} d-flex`}>
+              <FaLightbulb
                 className="m-auto"
                 size={35}
                 style={{ display: "block" }}

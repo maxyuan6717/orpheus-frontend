7a8334191ea0df9293e9180359575a47dd6911af
Max Yuan <maxyuan6717@gmail.com>
2021-05-22 18:04:41 -0400

    Fix pageview tracking

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import Register from "./pages/register";
 import Interested from "./pages/interested";
 import NavMenu from "./components/menu";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
 import "./common/typography.css";
 import "./common/structure.css";
 import ReactGA from "react-ga";
-import { createBrowserHistory } from "history";
 import { GA_id } from "./util/base";
 
 ReactGA.initialize(GA_id);
 
-const history = createBrowserHistory();
-
-history.listen((location) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
-
 function App() {
   const MyRoute = ({ children, ...otherProps }) => {
+    const history = useHistory();
+    ReactGA.set({ page: history.location.pathname });
+    ReactGA.pageview(history.location.pathname);
     return (
       <Route {...otherProps}>
         <div className={styles.container}>

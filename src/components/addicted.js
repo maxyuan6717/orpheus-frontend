832f43bd99ff55ccf9094f8084eee0f885e13309
Max Yuan <maxyuan6717@gmail.com>
2021-05-23 21:01:10 -0400

    take out underscores in addicted section

--- src/components/addicted.js
+++ src/components/addicted.js
@@ -11,19 +11,19 @@ const Addicted = () => {
           <ul className={styles.list}>
             <li>
               <p>
-                <em>Yes?</em>_ Let's learn more about your relationship with
+                <em>Yes?</em> Let's learn more about your relationship with
                 technology.
               </p>
             </li>
             <li>
               <p>
-                <em>Maybe?</em>_ We often find ourselves here. Try the
-                assessment and see if you need this.
+                <em>Maybe?</em> We often find ourselves here. Try the assessment
+                and see if you need this.
               </p>
             </li>
             <li>
               <p>
-                <em>No?</em>_ Great. You may be interested in the rest of the
+                <em>No?</em> Great. You may be interested in the rest of the
                 material and are welcome to check it out. If you ever think
                 differently you can come back.
               </p>
